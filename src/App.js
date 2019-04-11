f2e48aff81004aab7aaf8d0f34f68889c9a21238
rckrieger <rebecca.c.krieger@gmail.com>
2019-04-11 05:46:44 -0700

    initial setup
    front end not arranged but basic backbones are there

--- src/App.js
+++ src/App.js
@@ -1,28 +1,154 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Project from './Project';
 
+import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
+import khoa from './khoa.jpg';
+
+
+
+var summercamp = {
+    name: "C# Coding Camp",
+    photos: [khoa],
+    projectSummary: "When I was researching Seattle to prepare for my Microsoft internship I noticed the area had many free high school camps but very few middle school ones, and being a tech hub the city had an abundance of interns who could code. So I thought why not give the interns the opportunity to teach and the girls the opportunity to learn to code this summer? I teamed up with interns from Microsoft, Amazon, and Google interns to teach 40 girls and allies C# (http://codecadets.wixsite.com/ home). What made the camp unique was besides being the only free coding camp for middle schoolers in the area, we introduced the students to full time engineers and PMs who are thriving in tech via the Q&As so they can see that they could grow up to succeed too. ",
+    problemSpace: "Computer Science in Low Income Communities",
+    criteriaForSuccess:["","b","c"],
+    impact: "Taught 30 middle schoolers",
+    lessonsLearned: ["Plan guest speakers ahead of time", "delegate", "seperated internal and extrernal communication"],
+    doDifferently: ["Plan less  currculum", "Being right"],
+    goals: ["Get where I am going fast", "Experience the most I can on fieldtrips"],
+    budget: 4000,
+    timeline: "",
+    feedback:[],
+    psychReq: [["Service Project consisting of such activities as creating a manual for a community organization, creating/organizing an event or workshop for a community organization, or making an educational product",
+    "created a summer camp with 7 sessions"]],
+    csReq: [["Technical accomplishment: The technical results presented in the senior\n" +
+    "project should reflect the maturity of a senior level student in both the\n" +
+    "depth to which the subject is developed and in the degree to which a\n" +
+    "variety of information or viewpoints is brought to bear on the problem.", "This website?"],
+    ["Diligent execution: The senior project will be independent work by the\n" +
+    "student, developed under his or her own initiative and on a timetable\n" +
+    "under the student's control. The instructor who supervises the senior\n" +
+    "project is expected to supply only general advice and guidance and to help\n" +
+    "the student avoid pitfalls and blunders; the day-to-day activities which\n" +
+    "affect the execution of the project are up to the student.", "See Timetable Above"],
+    ["Effective communication: The senior project should represent a well\n" +
+    "polished piece of writing, with careful consideration given to presenting\n" +
+    "the results of the investigation in such a way as to provide a maximum of\n" +
+    "information with a minimum of effort required of the reader. Standard\n" +
+    "procedures of footnoting, referencing, and symbol usage as used in the\n" +
+    "technical literature should be employed. The final project document\n" +
+    "should give evidence that the student has acquired the ability to skillfully\n" +
+    "communicate on a technical subject to an audience less knowledgeable\n" +
+    "than the author. All computer programs are to be completely and fully\n" +
+    "documented.","see emails bellow and this github"]]
+};
+var industryImpact = {
+    name: "Creating Industry Change Bottom-up",
+    photos: ["khoa.JPG"],
+    projectSummary: "",
+    problemSpace: "When my dad tried signing my sisters up for coding summercamp, he only found offerings",
+    impact: "Taught 30 middle schoolers",
+    criteriaForSuccess:["a","b","c"],
+    lessonsLearned: ["Plan guest speakers ahead of time", "delegate", "seperated internal and extrernal communication"],
+    doDifferently: ["Plan less  currculum", "Being right"],
+    goals: ["Get where I am going fast", "Experience the most I can on fieldtrips"],
+    budget: 0,
+    timeline: "",
+    feedback: [],
+    psychReq: [["Service Project consisting of such activities as creating a manual for a community organization, creating/organizing an event or workshop for a community organization, or making an educational product",
+        "created a summer camp with 7 sessions"]],
+    csReq: [["Technical accomplishment: The technical results presented in the senior\n" +
+    "project should reflect the maturity of a senior level student in both the\n" +
+    "depth to which the subject is developed and in the degree to which a\n" +
+    "variety of information or viewpoints is brought to bear on the problem.", "This website?"],
+        ["Diligent execution: The senior project will be independent work by the\n" +
+        "student, developed under his or her own initiative and on a timetable\n" +
+        "under the student's control. The instructor who supervises the senior\n" +
+        "project is expected to supply only general advice and guidance and to help\n" +
+        "the student avoid pitfalls and blunders; the day-to-day activities which\n" +
+        "affect the execution of the project are up to the student.", "See Timetable Above"],
+        ["Effective communication: The senior project should represent a well\n" +
+        "polished piece of writing, with careful consideration given to presenting\n" +
+        "the results of the investigation in such a way as to provide a maximum of\n" +
+        "information with a minimum of effort required of the reader. Standard\n" +
+        "procedures of footnoting, referencing, and symbol usage as used in the\n" +
+        "technical literature should be employed. The final project document\n" +
+        "should give evidence that the student has acquired the ability to skillfully\n" +
+        "communicate on a technical subject to an audience less knowledgeable\n" +
+        "than the author. All computer programs are to be completely and fully\n" +
+        "documented.","see emails bellow and this github"]]
+};
+
+var conferences = {
+    name: "Conference Bootcamp",
+    photos: ["khoa.JPG"],
+    projectSummary: "",
+    problemSpace: "When my dad tried signing my sisters up for coding summercamp, he only found offerings",
+    impact: "Taught 30 middle schoolers",
+    criteriaForSuccess:["a","b","c"],
+    lessonsLearned: ["Plan guest speakers ahead of time", "delegate", "seperated internal and extrernal communication"],
+    doDifferently: ["Plan less  currculum", "Being right"],
+    goals: ["Get where I am going fast", "Experience the most I can on fieldtrips"],
+    budget: 0,
+    timeline:"",
+    feedback: [],
+    psychReq: [["Service Project consisting of such activities as creating a manual for a community organization, creating/organizing an event or workshop for a community organization, or making an educational product",
+        "created a summer camp with 7 sessions"]],
+    csReq: [["Technical accomplishment: The technical results presented in the senior\n" +
+    "project should reflect the maturity of a senior level student in both the\n" +
+    "depth to which the subject is developed and in the degree to which a\n" +
+    "variety of information or viewpoints is brought to bear on the problem.", "This website?"],
+        ["Diligent execution: The senior project will be independent work by the\n" +
+        "student, developed under his or her own initiative and on a timetable\n" +
+        "under the student's control. The instructor who supervises the senior\n" +
+        "project is expected to supply only general advice and guidance and to help\n" +
+        "the student avoid pitfalls and blunders; the day-to-day activities which\n" +
+        "affect the execution of the project are up to the student.", "See Timetable Above"],
+        ["Effective communication: The senior project should represent a well\n" +
+        "polished piece of writing, with careful consideration given to presenting\n" +
+        "the results of the investigation in such a way as to provide a maximum of\n" +
+        "information with a minimum of effort required of the reader. Standard\n" +
+        "procedures of footnoting, referencing, and symbol usage as used in the\n" +
+        "technical literature should be employed. The final project document\n" +
+        "should give evidence that the student has acquired the ability to skillfully\n" +
+        "communicate on a technical subject to an audience less knowledgeable\n" +
+        "than the author. All computer programs are to be completely and fully\n" +
+        "documented.","see emails bellow and this github"]]
+};
+
+
+const PROJECTS = [summercamp,industryImpact, conferences];
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  }
   render() {
+    const activePlace = this.state.activePlace;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav
+          variant="tabs"
+          defaultActiveKey={activePlace}
+          onSelect={index => {
+            this.setState({ activePlace: index });
+          }}
+        >
+          {PROJECTS.map((proj, index) => (
+              <Nav.Item>
+                <Nav.Link key={index} eventKey={index}>{proj.name}</Nav.Link>
+              </Nav.Item>
+          ))}
+      </Nav>
+      <Project proj={PROJECTS[activePlace]}/>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/Project.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import CodingCampTimeline from './CodingCampTimeline';
+import Radar from 'react-d3-radar';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootswatch/spacelab/bootstrap.css";
+import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
+import khoa from './khoa.jpg'
+
+class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.props = {
+      proj: props.proj,
+    };
+  }
+
+  render() {
+    return (
+      <div className="ProjectBox">
+        <Container>
+          <Row>
+            <div className="Name">
+              <h1>{this.props.proj.name}</h1>
+            </div>
+          </Row>
+          <Row>
+            <Col md={4} sm={4}>
+              <div className = "Photos">
+                {this.props.proj.photos.map((photo, index) => (
+                  <img src= {photo} alt= "picture"/>
+                ))}
+              </div>
+            </Col>
+            <Col md={8} sm={8}>
+              <div className = "projectSummary" align="left">
+                <h3>Project Summary</h3>
+                  <p>{this.props.proj.projectSummary}</p>
+              </div>
+            </Col>
+            <div className= "ProblemSpace" align="left">
+              <h3>Problem Space</h3>
+              <p>{this.props.proj.problemSpace}</p>
+            </div>
+          </Row>
+          <Row>
+            <div className= "KeyPerformaceIndicators" align="left">
+              <h3>Criteria For Success</h3>
+              <p>{this.props.proj.criteriaForSuccess}</p>
+            </div>
+            <div className= "Impact" align="left">
+              <h3>Impact</h3>
+              <p>{this.props.proj.impact}</p>
+            </div>
+            <div className= "LessonsLearned" align="left">
+              <h3>Lessons Learned</h3>
+              {this.props.proj.lessonsLearned.map((lessons, index) => (
+              <p>"{lessons}"</p>
+            ))}
+            </div>
+            <div className="DoDifferently" align="left">
+              <h3>What I Would Do Differently</h3>
+              {this.props.proj.doDifferently.map((different, index) => (
+              <p>"{different}"</p>
+            ))}
+
+            </div>
+            <div className="Goals">
+              <h3>Goals</h3>
+              {this.props.proj.goals.map((goal, index) => (
+                <h5>"{goal}"</h5>
+              ))}
+            </div>
+            <div className="Timeline" align="left">
+              <h3>Timeline</h3>
+              <p>{this.props.proj.timeline}</p>
+            </div>
+            <div className="Feedback" align="left">
+              <h3>Feedback</h3>
+              {this.props.proj.feedback.map((review, index) =>
+                (
+                  <img src= {review} alt= "id pic"/>
+                ))
+              }
+            </div>
+          </Row>
+          <Row>
+            <CodingCampTimeline/>
+          </Row>
+          <Row>
+            <div lassName="Requirements" align="left">
+              <Col md={6} sm={6}>
+                <h3>Psych Requirements</h3>
+                {this.props.proj.psychReq.map((slot, index) =>
+                    (<p>{slot[0]} : {slot[1]}</p>)
+                )
+                }
+              </Col>
+              <Col md={6} sm={6}>
+                <h3>Computer Science Requirements</h3>
+                {this.props.proj.csReq.map((slot, index) =>
+                    (<p>{slot[0]} : {slot[1]}</p>)
+                )
+                }
+              </Col>
+            </div>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Project;
